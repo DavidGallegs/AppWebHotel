f141f471fe96aaf92593c06b2f964d07ff78cef5
Guillermo <nieblarobertguillermo@gmail.com>
2026-05-24 21:05:26 +0200

    datos de cerrar sesion

--- frontend/src/components/sesiones/LogoutButton.tsx
+++ frontend/src/components/sesiones/LogoutButton.tsx
@@ -1,8 +1,29 @@
 import { signOut } from 'auth-astro/client';
+
 export default function LogoutButton() {
+
   const handleLogout = async () => {
-    const options = { callbackUrl: '/login' };
-    await signOut(options as any);
+  await fetch('http://localhost:8000/api/logout', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+
+    localStorage.removeItem('token');
+
+    await signOut();
+
+    window.location.href = '/login';
   };
-  return <button onClick={handleLogout} className="logout-btn" aria-label="Cerrar la sesión actual">Cerrar Sesión</button>;
+
+  return (
+    <button
+      onClick={handleLogout}
+      className="logout-btn"
+      aria-label="Cerrar la sesión actual"
+    >
+      Cerrar Sesión
+    </button>
+  );
 }
