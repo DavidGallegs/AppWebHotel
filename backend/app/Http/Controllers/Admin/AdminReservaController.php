b9d950ea1141d978a43c2065feaabcd17b4d4657
Guillermo <nieblarobertguillermo@gmail.com>
2026-05-25 06:37:15 +0200

    habitacion

--- backend/app/Http/Controllers/Admin/AdminReservaController.php
+++ backend/app/Http/Controllers/Admin/AdminReservaController.php
@@ -225,8 +225,8 @@ class AdminReservaController extends Controller
                 // ======================
                 if (!empty($datos['idHabitacion']) && !empty($datos['numPersonas'])) {
 
-                    $reserva->habitaciones()->sync([
-                        (int) $datos['idHabitacion'] => [
+                    $reserva->habitaciones()->syncWithoutDetaching([
+                        $datos['idHabitacion'] => [
                             'numPersonas' => $datos['numPersonas']
                         ]
                     ]);
