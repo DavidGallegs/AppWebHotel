98acb33e1e0ef066eb0dfe829b3d355261a27fdc
Guillermo <nieblarobertguillermo@gmail.com>
2026-05-27 12:56:16 +0200

    arreglo de errores

--- frontend/src/components/formularios/reservaHotel/SeccionTitular.tsx
+++ frontend/src/components/formularios/reservaHotel/SeccionTitular.tsx
@@ -1,5 +1,5 @@
 import { useFormContext, useWatch } from "react-hook-form";
-import { listaPaises } from "../utils/paises"; 
+import { listaPaises } from "../utils/paises";
 import { type TReserva } from "./esquemaReserva";
 import { BuscadorMunicipio } from "../utils/BuscadorMunicipio";
 
@@ -17,171 +17,178 @@ export function SeccionTitular({ isAdmin = false, userEmail = "" }: Props) {
         name: "titular.pais"
     });
 
-    // Si NO es admin y le pasamos un correo, forzamos ese correo
     if (!isAdmin && userEmail) {
-        setValue('titular.correo', userEmail);
+        setValue("titular.correo", userEmail);
     }
 
     return (
-
         <fieldset className="seccion-titular">
-
             <legend>Datos del Titular y Reserva</legend>
 
+            {/* Nº PERSONAS */}
             <div className="input-group">
-
                 <label htmlFor="num-personas">
                     Número total de personas en la reserva:
                 </label>
 
                 <select
                     id="num-personas"
-                    {...register("numPersonas", {
-                        valueAsNumber: true
-                    })}
+                    {...register("numPersonas", { valueAsNumber: true })}
                 >
                     <option value="1">1 Persona</option>
                     <option value="2">2 Personas</option>
                     <option value="3">3 Personas</option>
                 </select>
-
             </div>
 
-            <input
-                type="hidden"
-                value="TI"
-                {...register("titular.rol")}
-            />
+            <input type="hidden" value="TI" {...register("titular.rol")} />
 
-            {/* ERROR 1 -> INPUT SIN NOMBRE ACCESIBLE */}
-            <input
-                type="text"
-                {...register("titular.nombre")}
-            />
+            {/* NOMBRE */}
+            <div className="input-group">
+                <label htmlFor="titular-nombre">Nombre</label>
+                <input
+                    id="titular-nombre"
+                    type="text"
+                    {...register("titular.nombre")}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.apellido1")}
-            />
+            {/* APELLIDO 1 */}
+            <div className="input-group">
+                <label htmlFor="titular-apellido1">Primer apellido</label>
+                <input
+                    id="titular-apellido1"
+                    type="text"
+                    {...register("titular.apellido1")}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.apellido2")}
-            />
+            {/* APELLIDO 2 */}
+            <div className="input-group">
+                <label htmlFor="titular-apellido2">Segundo apellido</label>
+                <input
+                    id="titular-apellido2"
+                    type="text"
+                    {...register("titular.apellido2")}
+                />
+            </div>
 
-            {/* ERROR 2 -> SELECT SIN LABEL NI ARIA-LABEL */}
-            <select {...register("titular.tipoDocumento")}>
+            {/* TIPO DOCUMENTO */}
+            <div className="input-group">
+                <label htmlFor="tipo-documento">Tipo de documento</label>
 
-                <option value="">
-                    Tipo Documento...
-                </option>
+                <select
+                    id="tipo-documento"
+                    {...register("titular.tipoDocumento")}
+                >
+                    <option value="">Selecciona tipo...</option>
+                    <option value="DNI">DNI</option>
+                    <option value="NIF">NIF</option>
+                    <option value="PASSPORT">Pasaporte</option>
+                </select>
+            </div>
 
-                <option value="DNI">
-                    DNI
-                </option>
+            {/* Nº DOCUMENTO */}
+            <div className="input-group">
+                <label htmlFor="num-documento">Número de documento</label>
+                <input
+                    id="num-documento"
+                    type="text"
+                    {...register("titular.numeroDocumento")}
+                />
+            </div>
 
-                <option value="NIF">
-                    NIF
-                </option>
+            {/* SOPORTE */}
+            <div className="input-group">
+                <label htmlFor="soporte-documento">Soporte documento</label>
+                <input
+                    id="soporte-documento"
+                    type="text"
+                    {...register("titular.soporteDocumento")}
+                />
+            </div>
 
-                <option value="PASSPORT">
-                    Pasaporte
-                </option>
+            {/* FECHA NACIMIENTO */}
+            <div className="input-group">
+                <label htmlFor="fecha-nacimiento">Fecha de nacimiento</label>
+                <input
+                    id="fecha-nacimiento"
+                    type="date"
+                    {...register("titular.fechaNacimiento")}
+                />
+            </div>
 
-            </select>
+            {/* TELÉFONO */}
+            <div className="input-group">
+                <label htmlFor="telefono">Teléfono</label>
+                <input
+                    id="telefono"
+                    type="tel"
+                    {...register("titular.telefono")}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.numeroDocumento")}
-            />
+            {/* CORREO */}
+            <div className="input-group">
+                <label htmlFor="correo">Correo electrónico</label>
+                <input
+                    id="correo"
+                    type="email"
+                    {...register("titular.correo")}
+                    readOnly={!isAdmin}
+                    className={!isAdmin ? "campo-bloqueado" : ""}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.soporteDocumento")}
-            />
+            {/* DIRECCIÓN */}
+            <div className="input-group">
+                <label htmlFor="direccion">Dirección</label>
+                <input
+                    id="direccion"
+                    type="text"
+                    {...register("titular.direccion")}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="date"
-                {...register("titular.fechaNacimiento")}
-            />
+            {/* CP */}
+            <div className="input-group">
+                <label htmlFor="cp">Código Postal</label>
+                <input
+                    id="cp"
+                    type="text"
+                    {...register("titular.codigoPostal")}
+                />
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="tel"
-                {...register("titular.telefono")}
-            />
+            {/* PAÍS */}
+            <div className="input-group">
+                <label htmlFor="pais">País</label>
 
-            {/* ERROR 1 */}
-            <input
-                type="email"
-                {...register("titular.correo")}
-                readOnly={!isAdmin}
-                className={!isAdmin ? "campo-bloqueado" : ""}
-            />
+                <select id="pais" {...register("titular.pais")}>
+                    <option value="">Selecciona País...</option>
+                    {listaPaises.map((pais) => (
+                        <option key={pais.codigo3} value={pais.codigo3}>
+                            {pais.nombre}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.direccion")}
-            />
-
-            {/* ERROR 1 */}
-            <input
-                type="text"
-                {...register("titular.codigoPostal")}
-            />
-
-            {/* ERROR 2 */}
-            <select {...register("titular.pais")}>
-
-                <option value="">
-                    Selecciona País...
-                </option>
-
-                {listaPaises.map((pais) => (
-
-                    <option
-                        key={pais.codigo3}
-                        value={pais.codigo3}
-                    >
-                        {pais.nombre}
-                    </option>
-
-                ))}
-
-            </select>
-
+            {/* MUNICIPIO */}
             {paisSeleccionado === "ESP" ? (
-
                 <div className="campo-municipio-largo">
-
-                    <BuscadorMunicipio
-                        name="titular.codigoMunicipio"
-                    />
-
+                    <BuscadorMunicipio name="titular.codigoMunicipio" />
                 </div>
-
             ) : (
-
                 <div className="campo-municipio-largo">
-
-                    {/* ERROR 1 */}
+                    <label htmlFor="municipio">Municipio</label>
                     <input
+                        id="municipio"
                         type="text"
                         {...register("titular.nombreMunicipio")}
                     />
-
                 </div>
-
             )}
-
         </fieldset>
-
     );
-
 }
